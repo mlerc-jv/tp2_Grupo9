4d4695b058d739e08d20220fffe349ba73f38cda
mlerc-jv <jimenavirmeru@gmail.com>
2026-08-25 14:32:39 -0300

    Agrego ejercicio 9 - Meruvia Jimena

--- tp2_Grupo9/src/ar/edu/unju/escmi/tp2/ejercicio9/Ejercicio9.java
+++ tp2_Grupo9/src/ar/edu/unju/escmi/tp2/ejercicio9/Ejercicio9.java
@@ -1,6 +1,6 @@
-package ar.edu.unju.escmi.tp2.ejercicio4;
+package ar.edu.unju.escmi.tp2.ejercicio9;
 
-public class Ejercicio4 {
+public class Ejercicio9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
