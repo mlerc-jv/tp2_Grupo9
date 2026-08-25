6a4b8ba099191d461d9b80549475b0474878190b
antoto17 <tom510betacode@gmail.com>
2026-08-25 17:48:18 -0300

    Ejercicio 3

---
+++ tp2_Grupo9/src/ar/edu/unju/escmi/tp2/ejercicio3/Ejercicio3.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.escmi.tp2.ejercicio3;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int CODIGO_POSTAL_JUJUY = 4600;
+        final double PI = 3.1415926535;
+        final int DIAS_SEMANA = 7;
+        final int MESES_AÑO = 12;
+
+        System.out.println("Código postal de Jujuy: " + CODIGO_POSTAL_JUJUY);
+        System.out.println("Valor de PI: " + PI);
+        System.out.println("Cantidad de días de la semana: " + DIAS_SEMANA);
+        System.out.println("Cantidad de meses del año: " + MESES_AÑO);
+	}
+
+}
