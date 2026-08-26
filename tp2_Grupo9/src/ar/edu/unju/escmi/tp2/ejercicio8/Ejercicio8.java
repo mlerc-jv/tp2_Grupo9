993030092330552d66df6412b1bcef4ead2377ec
antoto17 <tom510betacode@gmail.com>
2026-08-26 15:05:47 -0300

    Ejercicio 8 - Maidana Antonella

---
+++ tp2_Grupo9/src/ar/edu/unju/escmi/tp2/ejercicio8/Ejercicio8.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.escmi.tp2.ejercicio8;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5, b = 10, c = 2, d = 6;
+
+		System.out.println("Valores antes de las operaciones:");
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+
+		a += 5;
+		b -= 2;
+		c *= 3;
+		d /= 2;
+
+		System.out.println("Valores despues de las operaciones:");
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+	}
+
+}
