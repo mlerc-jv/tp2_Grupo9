4b58c66024053d9ebb80c88a5a07fbe0408f0fff
Carrillo Marianela Valentina Abril <carrillomarianelaabril@gmail.com>
2026-08-26 20:40:37 -0300

    Ejercicio 2 - Carrillo Abril

---
+++ tp2_Grupo9/src/ar/edu/unju/escmi/tp2/ejercicio2/Ejercicio2.java
@@ -0,0 +1,36 @@
+package ar.edu.unju.escmi.tp2.ejercicio2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nombre1 = "Marianela";
+		String apellido1 = "Carrillo";
+		int dni1 = 48142585;
+		
+		String nombre2 = "Antonella";
+		String apellido2 = "Maidana";
+		int dni2 = 48141988;
+		
+		String nombre3 = "Jimena";
+		String apellido3 = "Meruvia";
+		int dni3 = 48910310;
+		
+		String nombre4 = "Julieta";
+		String apellido4 = "Ortega";
+		int dni4 = 48276209;
+		
+		String nombre5 = "Octavio";
+		String apellido5 = "Lamas";
+		int dni5 = 48955280;
+		
+		System.out.println(nombre1 + " " + apellido1 + " - " + dni1);
+		System.out.println(nombre2 + " " + apellido2 + " - " + dni2);
+		System.out.println(nombre3 + " " + apellido3 + " - " + dni3);
+		System.out.println(nombre4 + " " + apellido4 + " - " + dni4);
+		System.out.println(nombre5 + " " + apellido5 + " - " + dni5);
+		
+	}
+
+}
