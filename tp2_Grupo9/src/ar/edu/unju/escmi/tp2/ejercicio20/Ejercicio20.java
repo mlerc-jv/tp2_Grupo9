7dbf3ead1077df89d98c839c3ca46d8c6cd28e91
Carrillo Marianela Valentina Abril <carrillomarianelaabril@gmail.com>
2026-08-27 00:44:30 -0300

    Ejercicio 20 - Carrillo Abril

---
+++ tp2_Grupo9/src/ar/edu/unju/escmi/tp2/ejercicio20/Ejercicio20.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.escmi.tp2.ejercicio20;
+
+public class Ejercicio20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String textoOriginal = "Hoja";
+		String textoModificado = textoOriginal.replace("ja", "la");
+		
+		System.out.println(textoOriginal);
+		System.out.println(textoModificado);
+	}
+
+}
